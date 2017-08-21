57d4aa225f14c6867280eaf44395bf7767695b3d
Arturo Cordero <vakmancordero@gmail.com>
2017-08-21 14:00:23 -0500

    Añadiendo proyecto

---
+++ src/main/java/com/candlelabs/inventory/ProductController.java
@@ -0,0 +1,18 @@
+package com.candlelabs.inventory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ProductController implements Initializable {
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+    
+    
+}
